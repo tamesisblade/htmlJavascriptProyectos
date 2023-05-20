8d783cc4cbab61186b0eb8cd5177669352c8d9a2
tamesisblade <53984711+tamesisblade@users.noreply.github.com>
2023-05-20 08:09:07 -0500

    apeend child

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -1,8 +1,9 @@
 //variables 
 const carrito           = document.querySelector("#carrito")
-const contenedorCarrito = document.querySelector("lista-carrito tbody")
-const vaciarCarriboBtn  = document.querySelector("vaciar-carrito")
+const contenedorCarrito = document.querySelector("#lista-carrito tbody")
+const vaciarCarriboBtn  = document.querySelector("#vaciar-carrito")
 const listaCursos       = document.querySelector("#lista-cursos")
+let articulosCarrito    = []
 cargarEventListeners()
 function cargarEventListeners(){
     //cuando agregar un curso presionando "agregar al carrito"
@@ -28,5 +29,41 @@ function leerDatosCurso(curso){
     c.precio    = curso.querySelector(".precio span").innerHTML
     c.id        = curso.querySelector("a").getAttribute('data-id')
     c.cantidad  = 1
-    console.log("aaa",c)
+    articulosCarrito = [...articulosCarrito,c]
+    carritoHTML()
+}
+//Muestra el carrito de compras en el HTML
+function carritoHTML(){
+    //limpiar el HTML
+    limpiarHTML()
+    //Recorre el carrito y genera el HTML
+    console.log("carrito",articulosCarrito)
+    articulosCarrito.forEach(p => {
+        const { id, imagen, titulo, precio, cantidad } = p
+        const row = document.createElement("tr");
+        row.innerHTML = `
+         <td><img width="50" src="${ imagen }"/></td>
+         <td>${ titulo }</td>
+         <td>${ precio }</td>
+         <td>${ cantidad }</td>
+         <td>
+         <a href="#" class="borrar-curso" data-id="${ id }">X</a>
+         </td>
+        `;
+        console.log("raaaa",row)
+        console.log("res",contenedorCarrito)
+        //agrega el HTML del carrito en tbody
+        contenedorCarrito.appendChild(row)
+    })
+}
+
+//eliminar los cursos del tbody
+function limpiarHTML(){
+    //forma lenta
+    //contenedorCarrito.innerHTML = ""
+    //forma rapida
+    //mientras haya un hijo
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
 }
